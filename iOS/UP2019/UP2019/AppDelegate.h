c56ecfac0c33b60dd6fe0700ececea3545bf7914
yunis <332963965@qq.com>
2019-01-02 16:22:30 +0800

    first commit
    first commit

---
+++ iOS/UP2019/UP2019/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UP2019
+//
+//  Created by Yunis on 2019/1/2.
+//  Copyright © 2019年 Yunis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOS/UP2019/UP2019/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UP2019
+//
+//  Created by Yunis on 2019/1/2.
+//  Copyright © 2019年 Yunis. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
